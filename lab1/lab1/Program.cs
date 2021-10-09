b6e3d7ab8ce7f1aedead73e68c6e095c138d608b
Den1sBol <90184621+Den1sBol@users.noreply.github.com>
2021-10-09 09:29:19 +0300

    Add files via upload

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(5613);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(random.Next(0, 50));
+            }
+            Console.WriteLine("****************************");
+            Random random1 = new Random(5613);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(random1.Next(0, 50));
+            }
+            Console.WriteLine("****************************");
+            static byte[] GetCrypto(int length = 10)
+            {
+                var Gen = new RNGCryptoServiceProvider();
+                var Num = new byte[length];
+                Gen.GetBytes(Num);
+                return Num;
+            }
+            for (int i = 0; i <5;i++)
+            {
+                string crypto = Convert.ToBase64String(GetCrypto());
+                Console.WriteLine(crypto);
+            }
+        }
+    }
+}
