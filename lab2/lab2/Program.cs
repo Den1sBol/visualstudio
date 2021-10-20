3bed5288840e8d70f847367e00d1f430ecf3eb1b
Den1sBol <90184621+Den1sBol@users.noreply.github.com>
2021-10-20 19:45:36 +0300

    Add files via upload

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] Data = File.ReadAllBytes("lab2.txt").ToArray();
+            foreach(byte i in Data)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            var password = new RNGCryptoServiceProvider();
+            byte[] pass = new byte[Data.Length];
+            password.GetBytes(pass);
+            foreach (byte i in pass)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            byte[] enc = new byte[Data.Length];
+            for (int i = 0; i < Data.Length; i++)
+            {
+                enc[i] = (byte)(Data[i] ^ pass[i]);
+            }
+
+            File.WriteAllBytes("encrypt.dat", enc);
+            byte[] text = File.ReadAllBytes("encrypt.dat").ToArray();
+            byte[] dec = new byte[text.Length];
+            for (int i=0; i<text.Length;i++)
+            {
+                dec[i] = (byte)(text[i] ^ pass[i]);
+            }
+
+            Console.Write(Encoding.UTF8.GetString(dec));
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+    }
+}
