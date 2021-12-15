5b2f8dce786e8b31164c0195b200f77902929812
Den1sBol <90184621+Den1sBol@users.noreply.github.com>
2021-12-15 20:26:13 +0200

    Add files via upload

---
+++ lab12/lab12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter("Microsoft", Microsoft.Extensions.Logging.LogLevel.Warning)
+                    .AddFilter("System", Microsoft.Extensions.Logging.LogLevel.Warning)
+                    .AddFilter("Logging.Program", Microsoft.Extensions.Logging.LogLevel.Debug)
+                    .AddConsole()
+                    .AddEventLog()
+                    .AddDebug();
+            });
+            Microsoft.Extensions.Logging.ILogger logger = loggerFactory.CreateLogger<Program>();
+
+            logger.LogDebug(1, "The entry point (0) here", args);
+            logger.LogInformation(2, "The Example of logging information occurred at (0)", DateTime.UtcNow);
+            logger.LogWarning(3, "Example log warning ((0))", DateTime.UtcNow);
+
+            var str = Console.ReadLine();
+            logger.LotDebug(1, "The next text was entered by keyboard: (0)", str);
+
+        }
+    }
+}
